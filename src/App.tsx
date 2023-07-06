9548534be277aff31e14cc56cfb79c20652c1570
ChaDongmin <cdm1263@gmail.com>
2023-07-06 21:17:19 +0900

    FEAT : 기본 레이아웃 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
+import { styled } from 'styled-components';
+import Left from './components/Left';
+import Middle from './components/Middle';
+import Right from './components/Right';
+
 function App() {
-  return <h1>App</h1>;
+  return (
+    <Conatainer>
+      <LeftArea>
+        <Left />
+      </LeftArea>
+      <MiddleArea>
+        <Middle />
+      </MiddleArea>
+      <RightArea>
+        <Right />
+      </RightArea>
+    </Conatainer>
+  );
 }
+
+const Conatainer = styled.div`
+  display: flex;
+  height: 100vh;
+`;
+
+const LeftArea = styled.div`
+  width: 12%;
+`;
+const MiddleArea = styled.div`
+  width: 55%;
+`;
+const RightArea = styled.div`
+  width: 33%;
+`;
 
 export default App;

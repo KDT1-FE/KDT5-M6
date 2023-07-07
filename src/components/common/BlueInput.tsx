2d1fde8c74795a9e78fd97ab8dfd2385722a814d
ChaDongMin <cdm1263@gmail.com>
2023-07-07 16:18:08 +0900

    STYLE : 공용 Input 스타일링 수정

--- src/components/common/BlueInput.tsx
+++ src/components/common/BlueInput.tsx
@@ -1,19 +1,46 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 function BlueInput({ ...props }) {
   return <StyledInput {...props} />;
 }
 
-const StyledInput = styled.input`
-  outline: none;
-  padding: 6px;
-  height: 40px;
-  border-radius: 10px;
-  background: #f8f9fd;
+const StyledInput = styled.input<{
+  small?: boolean;
+  middle?: boolean;
+  large?: boolean;
+}>`
   border: none;
+  height: 40px;
+  outline: none;
+  color: #4464ff;
+  padding: 6px 12px;
+  border-radius: 40px;
+  background: #f8f9fd;
 
   &::placeholder {
     color: #a8b1ce;
   }
+
+  &:focus {
+    box-shadow: 0 0 3px #4464ff;
+  }
+
+  ${(props) =>
+    props.small &&
+    css`
+      width: 300px;
+    `}
+
+  ${(props) =>
+    props.middle &&
+    css`
+      width: 330px;
+    `}
+
+  ${(props) =>
+    props.large &&
+    css`
+      width: 408px;
+    `}
 `;
 
 export default BlueInput;

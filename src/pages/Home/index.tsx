4c5ef8077c79e9b341e6de358c7fb06794b8bffc
96uoow <mhs0704123@naver.com>
2023-07-14 16:59:12 +0900

    feat:일별소비량소비횟수기재

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -12,6 +12,7 @@ import Search from '@/pages/Home/Search';
 import getExpenses from '@/pages/Home/getExpenses';
 import MySkeleton from '@/components/MySkeleton';
 import { Value } from 'react-calendar/dist/cjs/shared/types';
+import { each } from 'chart.js/helpers';
 
 export default function Home() {
   const [edit] = useState(true);
@@ -54,21 +55,26 @@ export default function Home() {
     return [];
   }, [day, monthlyExpenses]);
 
-  const expenseDay = Object.keys(monthlyExpenses)
-  const a = expenseDay.map(item=>monthlyExpenses[item]
-    ? monthlyExpenses[item].map(x=>x.amount)
-    : null)
+  //1-31까지 배열에 넣음
+  const perDay = []
+  for(let i=1; i<32; i++){
+    perDay.push(i)
+  }
+  //해당 배열들을 monthlyExpenses에 넣고, 각 소비일의 첫번째, 데이터로된 배열을 만듦
+  const checkDay =
+  perDay.map(x=>monthlyExpenses[x] && monthlyExpenses[x].find(y => y))
+  .filter(z=>z !== undefined)
+
+  //배열 내에서 'YYYY-MM-DD'형식으로된 배열을 만듦
+  const expenseDay = checkDay.map(x=>x?.date.substring(0,10))
 
   // 총소비액
     const addContent = ({date} : any) => {
-      const expenseDay = Object.keys(monthlyExpenses)
-
+      const eachDay = moment(date).format('D')  
+      const dailyExpense = monthlyExpenses[eachDay]
       const contents = []
       //소비가 있는 날짜에만 값을 기입
-      if(expenseDay.find((day) => day === moment(date).format('D'))){
-
-        const eachDay = moment(date).format('D')  
-        const dailyExpense = monthlyExpenses[eachDay]
+      if(expenseDay.find((day) => day === moment(date).format('YYYY-MM-DD'))){
         const amountArray = dailyExpense.map(item=>item.amount)
         //총소비량
         const sum = amountArray.reduce((a,b)=>(a+b),0)

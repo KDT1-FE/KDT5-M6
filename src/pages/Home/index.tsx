fe79ba34b93e1251c55386154f7d3e3b863488e2
howooking <junsgk@gmail.com>
2023-07-18 13:09:08 +0900

    activeStartDate value에 반영

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,47 +3,39 @@ import { FloatButton } from 'antd';
 import DailyExpenseModal from '@/pages/Home/DailyExpenseModal';
 import '@/pages/Home/Calendar.scss';
 import { useEffect, useMemo, useState } from 'react';
-import formatDate from '@/utils/formatDateAndTime';
+import formatDateAndTime from '@/utils/formatDateAndTime';
 import moment from 'moment';
 import { MontlyExpensesType } from '@/types/expenses';
 import Calendar from 'react-calendar';
 import { PlusOutlined } from '@ant-design/icons';
-import Search from '@/pages/Home/Search';
 import getExpenses from '@/pages/Home/getExpenses';
 import MySkeleton from '@/components/MySkeleton';
 import { Value } from 'react-calendar/dist/cjs/shared/types';
-import { each } from 'chart.js/helpers';
 
 export default function Home() {
-  const [edit] = useState(true);
   const [list, setList] = useState(false);
-  const [selected] = useState('');
   const [toggleAdd, setToggleAdd] = useState(false);
+  const [value, setValue] = useState(new Date()); // 선택한 날짜
 
-  const [value, setValue] = useState(new Date());
   const day = useMemo(() => moment(value).format('D'), [value]);
   const month = useMemo(() => moment(value).format('M'), [value]);
   const year = useMemo(() => moment(value).format('YYYY'), [value]);
 
-  const [activeMonth, setActiveMonth] = useState(month)
-
-  const getActiveMonth = (activeStartDate : moment.MomentInput) => {
-    console.log(activeStartDate)
-    const newActiveMonth = moment(activeStartDate).format('M')
-    setActiveMonth(newActiveMonth)
-  }
-  console.log(activeMonth)
-
-  const [monthlyExpenses, setMonthlyExpenses] = useState<MontlyExpensesType>({});
+  const [monthlyExpenses, setMonthlyExpenses] = useState<MontlyExpensesType>(
+    {},
+  );
   const [dailyExpenseModalOpen, setDailyExpenseModalOpen] = useState(false);
-  const [addExpenseModalOpen, setaddExpenseModalOpen] = useState(false);
+  const [addExpenseModalOpen, setAddExpenseModalOpen] = useState(false);
+
+  // loading ui
   const [loading, setLoading] = useState(false);
 
+  // 월 소비 데이터 통신
   useEffect(() => {
     setLoading(true);
     const getData = async () => {
       try {
-        const response = await getExpenses(year, activeMonth); // 분기가 나눠짐
+        const response = await getExpenses(year, month);
         setMonthlyExpenses(response);
       } catch (error) {
         console.log(error);
@@ -52,71 +44,87 @@ export default function Home() {
       }
     };
     getData();
-  }, [activeMonth, year, list]);
+  }, [month, year, list]);
 
   const dailyExpenses = useMemo(() => {
     if (monthlyExpenses && monthlyExpenses[day]) {
       return monthlyExpenses[day].map((expense) => ({
         ...expense,
         key: expense._id,
-        time: formatDate(expense.date).time,
+        time: formatDateAndTime(expense.date).time,
       }));
     }
     return [];
   }, [day, monthlyExpenses]);
 
-  //1-31까지 배열에 넣음
-  const perDay = []
-  for(let i=1; i<32; i++){
-    perDay.push(i)
-  }
-  //해당 배열들을 monthlyExpenses에 넣고, 각 소비일의 첫번째 데이터로 된 배열을 만듦
-  const checkDay =
-  perDay.map(x=>monthlyExpenses[x] && monthlyExpenses[x].find(y => y))
-  .filter(z=>z !== undefined)
+  // // 1-31까지 배열에 넣음
+  // const perDay = [];
+  // for (let i = 1; i < 32; i++) {
+  //   perDay.push(i);
+  // }
+  // // 해당 배열들을 monthlyExpenses에 넣고, 각 소비일의 첫번째 데이터로 된 배열을 만듦
+  // const checkDay = perDay
+  //   .map((x) => monthlyExpenses[x] && monthlyExpenses[x].find((y) => y))
+  //   .filter((z) => z !== undefined);
 
-  //배열 내에서 'YYYY-MM-DD'형식으로된 배열을 만듦
-  const expenseDay = checkDay.map(x=>x?.date.substring(0,10))
+  // // 배열 내에서 'YYYY-MM-DD'형식으로된 배열을 만듦
+  // const expenseDay = checkDay.map((x) => x?.date.substring(0, 10));
+
+  const expenseDay = useMemo(
+    () =>
+      Object.keys(monthlyExpenses).map(
+        (day) => `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`,
+      ),
+    [month, monthlyExpenses, year],
+  );
 
   // 총소비액
-    const addContent = ({date} : any) => {
-      const eachDay = moment(date).format('D')  
-      const dailyExpense = monthlyExpenses[eachDay]
-      const contents = []
+  const addContents = ({ date }: { date: Date }) => {
+    const contents = [];
+    //소비가 있는 날짜에만 값을 기입
+    if (expenseDay.find((day) => day === moment(date).format('YYYY-MM-DD'))) {
+      const eachDay = moment(date).format('D');
+      const dailyExpense = monthlyExpenses[eachDay];
+      const dailyExpensesSum = dailyExpense
+        .map((item) => item.amount)
+        .reduce((a, b) => a + b, 0);
+      //총소비량
 
-      //소비가 있는 날짜에만 값을 기입
-      if(expenseDay.find((day) => day === moment(date).format('YYYY-MM-DD'))){
-        const amountArray = dailyExpense.map(item=>item.amount)
-        //총소비량
-        const sum = amountArray.reduce((a,b)=>(a+b),0)
-
-        contents.push(
-          <div key={day}>
-            <span>총소비 : {sum}원</span><br/>
-            <span>소비횟수 : {dailyExpense.length}회</span>
-          </div>
-        )
-      }
-      return <div>{contents}</div>
+      contents.push(
+        <div key={date.toISOString()}>
+          <span>총소비 : {dailyExpensesSum}원</span>
+          <br />
+          <span>소비횟수 : {dailyExpense.length}회</span>
+        </div>,
+      );
     }
+    return <>{contents}</>;
+  };
 
   return (
     <>
-    {loading
-      ? <div>
-          <MySkeleton/>
+      {loading ? (
+        <div>
+          <MySkeleton />
         </div>
-      : <Calendar
+      ) : (
+        <Calendar
           onChange={(value: Value) => {
             setValue(value as Date);
           }}
           value={value}
           onClickDay={() => setDailyExpenseModalOpen(true)}
-          tileContent={addContent}
+          tileContent={addContents}
+          formatDay={(_locale, date) => moment(date).format('D')}
           showNeighboringMonth={false}
-          onActiveStartDateChange={({activeStartDate})=>
-          getActiveMonth(activeStartDate)}/>
-    }
+          minDetail="month"
+          prev2Label={null}
+          next2Label={null}
+          onActiveStartDateChange={({ activeStartDate }) => {
+            setValue(activeStartDate!);
+          }}
+        />
+      )}
 
       <DailyExpenseModal
         month={month}
@@ -129,18 +137,16 @@ export default function Home() {
       />
       <ExpenditureForm
         open={addExpenseModalOpen}
-        setOpen={setaddExpenseModalOpen}
-        edit={edit}
+        setOpen={setAddExpenseModalOpen}
         list={list}
         setList={setList}
-        selected={selected}
         setToggleAdd={setToggleAdd}
       />
       <FloatButton
         type="primary"
         icon={<PlusOutlined />}
         onClick={() => {
-          setaddExpenseModalOpen(true);
+          setAddExpenseModalOpen(true);
         }}
       />
     </>

50f6cb0ca8d77379e1540249c656cd7674367061
96uoow <mhs0704123@naver.com>
2023-07-20 19:09:59 +0900

    소비금액별색상수정

--- src/utils/colorForExpense.ts
+++ src/utils/colorForExpense.ts
@@ -3,10 +3,14 @@ export const ExpensesColor = (a: number) => {
     return '#1fbf34'
   }else if(a>1000 && a<=10000){
     return '#ff8400'
-  }else if(a>10000 && a<=50000){
+  }else if(a>10000 && a<=30000){
     return '#27b4e6'
-  }else if(a>50000 && a<=100000){
+  }else if(a>30000 && a<=50000){
+    return '#8bed28'
+  }else if(a>50000 && a<=75000){
     return '#1d3dcc'
+  }else if(a>75000 && a<=100000){
+    return '#ffe047'
   }else if(a>100000 && a<=150000){
     return '#bb4af7'
   }else if(a>150000 && a<=200000){
